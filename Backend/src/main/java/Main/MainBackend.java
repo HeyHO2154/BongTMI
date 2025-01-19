e198a258d075c3bc658581099679ffcd12ca1c1e
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-01-19 20:54:13 +0900

    재정립

--- Backend/src/main/java/Main/MainBackend.java
+++ Backend/src/main/java/Main/MainBackend.java
@@ -1,6 +1,5 @@
 package Main;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
