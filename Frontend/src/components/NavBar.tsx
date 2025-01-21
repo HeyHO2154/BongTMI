4def70094712c64e7f69a72e06f37696ea341188
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-01-21 18:03:52 +0900

    아이콘 추가

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faSearch, faPlus, faEye, faUser, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const NavBarContainer = styled.nav`
   width: 100%; /* 부모 컨테이너의 100% 폭 */
@@ -19,7 +21,11 @@ const NavButton = styled(NavLink)`
   color: #555;
   font-size: 14px;
   text-align: center;
-  transition: color 0.3s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  transition: color 0.2s;
 
   &.active {
     color: #007bff;
@@ -29,11 +35,26 @@ const NavButton = styled(NavLink)`
 const NavBar: React.FC = () => {
   return (
     <NavBarContainer>
-      <NavButton to="/slider">Slider</NavButton>
-      <NavButton to="/search">Search</NavButton>
-      <NavButton to="/add-card">AddCard</NavButton>
-      <NavButton to="/feed">Feed</NavButton>
-      <NavButton to="/my-page">MyPage</NavButton>
+      <NavButton to="/slider">
+        <FontAwesomeIcon icon={faHeart} size="2x" />
+        공고 추천
+      </NavButton>
+      <NavButton to="/search">
+        <FontAwesomeIcon icon={faSearch} size="2x" />
+        공고 검색
+      </NavButton>
+      <NavButton to="/add-card">
+        <FontAwesomeIcon icon={faPlus} size="2x" />
+        공고 등록
+      </NavButton>
+      <NavButton to="/feed">
+        <FontAwesomeIcon icon={faEye} size="2x" />
+        후기 탐색
+      </NavButton>
+      <NavButton to="/my-page">
+        <FontAwesomeIcon icon={faUser} size="2x" />
+        마이페이지
+      </NavButton>
     </NavBarContainer>
   );
 };
