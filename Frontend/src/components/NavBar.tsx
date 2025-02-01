0cb8512338ee2928d6448439a1dbbbb519b33be8
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-01 16:42:47 +0900

    Update NavBar.tsx

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -64,7 +64,7 @@ const NavBar: React.FC = () => {
       </NavButton>
       <NavButton to="/feed" $isActive={activeButton === "/feed"}>
         <FontAwesomeIcon icon={faEye} size="2x" />
-        봉사 as
+        봉사 엉덩이
       </NavButton>
       <NavButton
         to="/my-page"
