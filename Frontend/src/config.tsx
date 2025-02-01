9c62ed75940cb6b0a4571cb399b5c81f3acd8421
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-01 18:55:54 +0900

    Update config.tsx

--- Frontend/src/config.tsx
+++ Frontend/src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
     //벡엔드 지향
-    API_DEV: "http://praven.kro.kr:25902",
-    API_DEV_C: "http://praven.kro.kr",
+    API_DEV: "https://praven.kro.kr:25902",
+    API_DEV_C: "https://praven.kro.kr",
 
     API_URL1: "http://localhost:8080",
     API_URL2: "http://praven.kro.kr:25902",
