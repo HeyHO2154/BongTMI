97d0207892179f1d8adbffc4ad3e93cb56ad965c
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-08 22:07:58 +0900

    Update LikeBongController.java

--- Backend/src/main/java/Main/User/LikeBong/LikeBongController.java
+++ Backend/src/main/java/Main/User/LikeBong/LikeBongController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "${Front_URL}")
 public class LikeBongController {
 	
 	@Autowired
