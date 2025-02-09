30f606da185a13796ae0dad815db11048e979566
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-09 14:01:01 +0900

    Update Swipe.tsx

--- Frontend/src/pages/Swipe.tsx
+++ Frontend/src/pages/Swipe.tsx
@@ -344,11 +344,11 @@ const Swipe: React.FC = () => {
                 <span
                   style={{
                     marginLeft: "10px", // 모집 마감일과 간격 조정
-                    backgroundColor: "rgba(255, 0, 0, 0.58)", // 빨간색 배경
+                    backgroundColor: "rgba(255, 0, 0, 0.81)", // 빨간색 배경
                     color: "white",
-                    padding: "5px 12px",
+                    padding: "6px 15px",
                     borderRadius: "6px",
-                    fontSize: "16px",
+                    fontSize: "18px",
                     fontWeight: "bold",
                     display: "inline-block",
                   }}
