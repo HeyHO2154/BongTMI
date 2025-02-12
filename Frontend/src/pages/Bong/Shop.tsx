2d6985d91095741b3e2d95098423a5a9e9ac6645
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-12 21:30:59 +0900

    Update Shop.tsx

--- Frontend/src/pages/Bong/Shop.tsx
+++ Frontend/src/pages/Bong/Shop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { ShoppingBag, Heart, Star, Truck, Package, Timer } from "lucide-react";
 
