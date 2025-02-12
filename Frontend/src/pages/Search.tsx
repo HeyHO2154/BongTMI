8704e19b257a05ab6a20dd94f33c03471b7fe6fa
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-12 13:15:53 +0900

    Update Search.tsx

--- Frontend/src/pages/Search.tsx
+++ Frontend/src/pages/Search.tsx
@@ -400,7 +400,6 @@ const Wrapper = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-  min-height: 100vh; /* 최소 높이만 설정 → 내용이 많으면 자동으로 늘어남 */
   overflow-y: auto; /* ✅ 스크롤 가능하도록 추가 */
   position: relative;
   height: calc(100vh - 160px); /* TopBar + NavBar 높이 제외 */
