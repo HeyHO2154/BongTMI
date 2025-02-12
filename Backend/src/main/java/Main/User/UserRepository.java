772a174ff18d73600029b6930b1f5754f77b9a64
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-12 18:18:25 +0900

    Update UserRepository.java

--- Backend/src/main/java/Main/User/UserRepository.java
+++ Backend/src/main/java/Main/User/UserRepository.java
@@ -12,21 +12,23 @@ import Main.Feed.Feed;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    // 사용자가 작성한 봉사 공고 조회 (userId -> nanmmbyNmAdmn)
-    @Query("SELECT b FROM Bong b WHERE b.nanmmbyNmAdmn = :userId")
+    // userId로 nickname 조회
+    @Query("SELECT u.nickname FROM User u WHERE u.id = :userId")
+    String findNicknameById(@Param("userId") String userId);
+
+    // 사용자가 작성한 봉사 공고 조회 (nickname으로 조회)
+    @Query("SELECT b FROM Bong b WHERE b.nanmmbyNmAdmn = (SELECT u.nickname FROM User u WHERE u.id = :userId)")
     List<Bong> findWrittenBongs(@Param("userId") String userId);
 
     // 사용자가 좋아요/신청한 봉사 공고 조회
-    @Query("SELECT b FROM Bong b WHERE b.progrmRegistNo IN " +
-           "(SELECT lb.bongId FROM LikeBong lb WHERE lb.userId = :userId)")
+    @Query("SELECT b FROM Bong b WHERE b.progrmRegistNo IN " + "(SELECT lb.bongId FROM LikeBong lb WHERE lb.userId = :userId)")
     List<Bong> findLikedBongs(@Param("userId") String userId);
 
-    // 사용자가 작성한 피드 조회 (userId -> author)
-    @Query("SELECT f FROM Feed f WHERE f.author = :userId")
+    // 사용자가 작성한 피드 조회 (nickname으로 조회)
+    @Query("SELECT f FROM Feed f WHERE f.author = (SELECT u.nickname FROM User u WHERE u.id = :userId)")
     List<Feed> findWrittenFeeds(@Param("userId") String userId);
 
     // 사용자가 좋아요한 피드 조회
-    @Query("SELECT f FROM Feed f WHERE f.feedID IN " +
-           "(SELECT lf.feedId FROM LikeFeed lf WHERE lf.userId = :userId)")
+    @Query("SELECT f FROM Feed f WHERE f.feedID IN " + "(SELECT lf.feedId FROM LikeFeed lf WHERE lf.userId = :userId)")
     List<Feed> findLikedFeeds(@Param("userId") String userId);
 }
