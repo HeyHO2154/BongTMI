99bb25f8922592ee2a4fbd15039a36a89ab1ea87
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2025-02-12 21:39:32 +0900

    Update MyPage.tsx

--- Frontend/src/pages/MyPage.tsx
+++ Frontend/src/pages/MyPage.tsx
@@ -548,7 +548,7 @@ const NoDataMessage = styled.div`
   justify-content: center;
   align-items: center;
   height: 200px;
-  font-size: 6rem;
+  font-size: 12rem;
   color: #868e96;
   text-align: center;
   background: white;
